cdeb2643ecc4f2e002ecef64dd5ec60239241020
shahzoor khan <49365063+shahzoor123@users.noreply.github.com>
2022-03-10 23:40:38 +0500

    80% template integrated

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,4 +4,21 @@ from django.shortcuts import render
 
 def index(request):
     return render (request, 'dashboard/index.html' )
+
+
+def login(request):
+    return render (request, 'dashboard/login.html' )
  
+def signup(request):
+    return render(request, 'dashboard/signup.html') 
+
+
+def contactus(request):
+    return render(request, 'dashboard/contactus.html')    
+
+
+def search (request):
+    return render (request, 'dashboard/search.html') 
+
+def calender (request):
+    return render (request, 'dashboard/calender.html')         
