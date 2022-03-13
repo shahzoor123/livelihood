fe4cda36aca5f643901f288a593d0662e38d8d3c
shahzoor khan <49365063+shahzoor123@users.noreply.github.com>
2022-03-13 23:56:55 +0500

    user model add

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+
+class User(AbstractBaseUser):
+    id = models.AutoField
+    username = models.CharField(max_length=200 , null=True)
+    email = models.EmailField(unique=True, null=True)
+   
