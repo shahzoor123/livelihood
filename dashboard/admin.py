440375e50fdf831c4fd581300368f94d20bc093e
shahzoor khan <49365063+shahzoor123@users.noreply.github.com>
2022-03-14 04:32:48 +0500

    models is created and ORM Mapping is done

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import User , School , District , Role
+
+admin.site.register(User)
+admin.site.register(School)
+admin.site.register(District)
+admin.site.register(Role)
+
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,47 @@
 from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.forms import CharField
 # Create your models here.
 
 class User(AbstractBaseUser):
-    id = models.AutoField
-    username = models.CharField(max_length=200 , null=True)
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100 , null=True)
     email = models.EmailField(unique=True, null=True)
+    password = models.CharField(max_length=50, null=True)
+    role = models.CharField(max_length=10 , null=True)
+    district_id = models.IntegerField (null=True)
+    school_id = models.IntegerField(null=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+
+class  Role(models.Model):
+    admin = models.CharField(max_length=50, null=True)
+    user = models.CharField(max_length=50 , null=True)
+    
+
+class School(models.Model):
+    school_id = models.AutoField(primary_key=True)
+    # school_name = models.CharField(max_length=100, null=True)
+    no_of_students = models.IntegerField()
+    no_of_girls = models.IntegerField()
+    no_of_boys = models.IntegerField()
+    add_user = models.ForeignKey(User, on_delete=models.SET_NULL , null =True)
+ 
+
+class District(models.Model):
+    district_id = models.AutoField(primary_key=True)
+    district_name = models.CharField(max_length=200, null=True)
+    no_of_schools = models.IntegerField()
+    add_school = models.ForeignKey(School, on_delete=models.SET_NULL , null =True)
+    add_user = models.ForeignKey(User, on_delete=models.SET_NULL , null =True)
+
+ 
+   
+
+  
+
    
