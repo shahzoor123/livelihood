65e60503de027ea50476dc731686fca3c81b1176
shahzoor khan <49365063+shahzoor123@users.noreply.github.com>
2022-03-21 23:16:12 +0500

    country chart integration

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('calender/', views.calender, name='calender'),
     path('chart/', views.chart, name='chart'),
+    path('chart2/', views.chart2, name='chart2'),
 
 ]
