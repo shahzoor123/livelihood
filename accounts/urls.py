0e7ef93dfad166b8ba8b26eef8829433d2727027
shahzoorkhan <shahzoorkhan321@gmail.com>
2022-03-24 00:44:36 +0500

    authentication pages updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.logedin, name='logedin'),
     path('logout', views.logout, name='logout'),
     path('login', views.logedin, name='logedin'),
     path('register', views.register, name='register'),
